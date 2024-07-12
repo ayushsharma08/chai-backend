669fe61df92af06c0b13a58b44a14ee8f4617ff0
Ayush Sharma <ayushsharma23082001@gmail.com>
2024-07-13 02:03:07 +0530

    all set up configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,14 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("pubic"))
 app.use(cookieParser())
 
+//routes
+
+import userRouer from "./routes/user.routes.js"
+
+
+//routes declaration
+// (for getting the router back we use middleware app.use not app.get)
+app.use("/api/v1/users", userRouer)
+
+
 export default app
